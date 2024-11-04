14ab9e942fac44d5367fe07dc6054b35e7582295
rayproj <rayproj@qq.com>
2024-11-04 11:52:19 +0800

    1.update

--- assets/GlobalSphereLight.ts
+++ assets/GlobalSphereLight.ts
@@ -1,5 +1,4 @@
-import { _decorator, Component, Node, SphereLight, v3 } from 'cc';
-import { Shader } from './shader/Shader';
+import { _decorator, Component, director, Node, Root, SphereLight, v3 } from 'cc';
 const { ccclass, property, executeInEditMode } = _decorator;
 
 @ccclass('GlobalSphereLight')
@@ -7,21 +6,23 @@ const { ccclass, property, executeInEditMode } = _decorator;
 export class GlobalSphereLight extends Component {
     private _sphereLight: SphereLight = null;
     private t_vec = v3();
+    private _root: Root = null;
 
     protected onLoad(): void {
         this._sphereLight = this.getComponent(SphereLight);
+        this._root = director.root;
     }
 
     protected onDisable(): void {
-        Shader.setGlobal(1, 0, 0, 0);
+       this._root.setShaderGlobal(1, 0, 0, 0);
     }
 
     update(deltaTime: number) {
         const pos = this.node.getPosition(this.t_vec);
         const lit = this._sphereLight;
         const color = lit.color;
-        Shader.setGlobal(0, pos.x, pos.y, pos.z, lit.range);
-        Shader.setGlobal(1, color.r / 255, color.g / 255, color.b / 255);
+        this._root.setShaderGlobal(0, pos.x, pos.y, pos.z, lit.range);
+        this._root.setShaderGlobal(1, color.r / 255, color.g / 255, color.b / 255);
     }
 }
 

9ed7231b4e342cfee113ab54fb1a8cb9d2f86396
rayproj <rayproj1@gmail.com>
2024-12-08 12:24:34 +0800

    1.增加水

--- assets/DepthTexture/DepthCamera.ts
+++ assets/DepthTexture/DepthCamera.ts
@@ -1,20 +1,21 @@
-import { _decorator, Camera, Component, director, gfx, Layers, Node, RenderTexture } from 'cc';
-const { ccclass, property } = _decorator;
+import { _decorator, Camera, Color, Component, director, gfx, Layers, Node, RenderTexture } from 'cc';
+const { ccclass, property, executeInEditMode } = _decorator;
 
 @ccclass('DepthCamera')
+// @executeInEditMode(true)
 export class DepthCamera extends Component {
     @property(RenderTexture)
     depthTexture: RenderTexture = null;
 
-    start() {
+    onLoad() {
         this.createDepthCamera();
     }
 
     private createDepthCamera() {
         const depthCameraNode = new Node('DepthCamera');
         this.node.addChild(depthCameraNode);
-        const depthCamera = depthCameraNode.addComponent(Camera);
         const mainCamera = this.getComponent(Camera);
+        const depthCamera = depthCameraNode.addComponent(Camera);
 
         // set
         depthCamera.priority = mainCamera.priority - 1;
@@ -24,10 +25,11 @@ export class DepthCamera extends Component {
         const depthTexture = this.depthTexture;
         const mCamera = mainCamera.camera;
         const scale = 1;
-        depthTexture.reset({ width: mCamera.width * scale, height: mCamera.height * scale });
+        // depthTexture.reset({ width: mCamera.width * scale, height: mCamera.height * scale });
+        depthTexture.resize(mCamera.width * scale, mCamera.height * scale);
         depthCamera.targetTexture = depthTexture;
 
-        depthCamera.clearColor = mainCamera.clearColor;
+        depthCamera.clearColor = Color.WHITE;
         depthCamera.clearDepth = mainCamera.clearDepth;
         depthCamera.clearStencil = mainCamera.clearStencil;
         depthCamera.projection = mainCamera.projection;
